7ce0e00f1e5430e53bbc6c54a7496041edb76ef4
Duerton <duerton@gmail.com>
2024-12-13 17:35:09 -0300

    add Default buttons

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,10 @@
 export const DEFAULT_FIRST_BUTTON_COLOR = '#6EADE1'
 export const DEFAULT_SECOND_BUTTON_COLOR = '#AF3D36'
 export const DEFAULT_THIRD_BUTTON_COLOR = '#625B71'
+
+export const DEFAULT_BUTTONS = [
+  { name:'Salvar', 
+    color: DEFAULT_FIRST_BUTTON_COLOR,
+    msg:''
+  },
+]
