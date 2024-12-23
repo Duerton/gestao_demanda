f757a1cc019f1e99a70eba52308282664af47974
Duerton <duerton@gmail.com>
2024-12-23 14:00:01 -0300

    add accordion to cadastro data

--- src/app/planejamento/[categoria]/[id]/page.tsx
+++ src/app/planejamento/[categoria]/[id]/page.tsx
@@ -1,5 +1,6 @@
-import { Box, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
 import Grid from '@mui/material/Grid2';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import FooterDefault from "@/components/FooterDefault";
 import { BUTTONS_PLANEJAMENTO, BUTTONS_PLANEJAMENTO_AUTORIZACAO, BUTTONS_PLANEJAMENTO_EDIT } from "@/utils/constants";
 import FileUploadAndList from "../../components/FileList";
@@ -34,156 +35,171 @@ async function Planejamento ( {
       <HeaderName name={header}/>
       <input type='hidden' value={estado} name="estado"></input>
       <Grid container spacing={2}>
-        <Grid size={6}>
-          <TextField 
-            fullWidth 
-            name="num_demanda" 
-            defaultValue={data.num_demanda}
-            label='Nº da demanda' 
-            disabled={isEdit}
-            />
+        <Grid size={12}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ArrowDownwardIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              <Typography>Dados de cadastro</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container spacing={2}>
+                <Grid size={6}>
+                  <TextField 
+                    fullWidth 
+                    name="num_demanda" 
+                    defaultValue={data.num_demanda}
+                    label='Nº da demanda' 
+                    disabled
+                    />
+                </Grid>
+                <Grid component="div" size={6}>
+                  <TextField 
+                    fullWidth 
+                    label='Data de registro' 
+                    name="data_registro" 
+                    defaultValue={data.data_registro}
+                    disabled
+                    />
+                </Grid>
+                <Grid size={12} component="div">
+                  <TextField 
+                    fullWidth 
+                    name="titulo" 
+                    label='Título' 
+                    defaultValue={data.titulo}
+                    disabled
+                    />
+                </Grid>
+                <Grid component="div" size={4}>
+                  <Box sx={{ minWidth: 120}} >
+                    <FormControl fullWidth>
+                      <InputLabel id='fornecedor'>Fornecedor</InputLabel>
+                      <Select
+                        id="fornecedor"
+                        label="Fornecedor"
+                        name="fornecedor"
+                        defaultValue={data.fornecedor || '1'}
+                        disabled
+                      >
+                        <MenuItem value={1}>Verde</MenuItem>
+                        <MenuItem value={2}>Amarelo</MenuItem>
+                        <MenuItem value={3}>Vermelho</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </Grid>
+                <Grid component="div" size={4}>
+                    <TextField 
+                      fullWidth 
+                      label='Data' 
+                      name="data_data" 
+                      defaultValue={data.data_data}
+                      disabled
+                      />
+                </Grid>
+                <Grid component="div" size={4}>
+                  <Box sx={{ minWidth: 120}}>
+                    <FormControl fullWidth>
+                      <InputLabel id='prioridade'>Prioridade</InputLabel>
+                      <Select
+                        id="prioridade"
+                        label="Prioridade"
+                        name="prioridade"
+                        defaultValue={data.prioridade || 'verde'}
+                        disabled
+                        >
+                        <MenuItem value={'verde'}>Verde</MenuItem>
+                        <MenuItem value={'amarelo'}>Amarelo</MenuItem>
+                        <MenuItem value={'vermelho'}>Vermelho</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </Grid>
+                <Grid component="div" size={6}>
+                  <TextField 
+                    fullWidth 
+                    label='Distrito'
+                    name="distrito"
+                    defaultValue={data.distrito}
+                    disabled
+                    />
+                </Grid>
+                <Grid component="div" size={6}>
+                  <TextField 
+                    fullWidth 
+                    label='Bairro'
+                    name="bairro"
+                    defaultValue={data.bairro}  
+                    disabled
+                    />
+                </Grid>
+                <Grid component="div" size={12}>
+                  <TextField 
+                    fullWidth 
+                    label='Logradouro' 
+                    name="logradouro"
+                    defaultValue={data.logradouro}  
+                    disabled
+                    />
+                </Grid>
+                <Grid component="div" size={6}>
+                  <Box sx={{ minWidth: 120}}>
+                    <FormControl fullWidth>
+                      <InputLabel id='programa'>Programa</InputLabel>
+                      <Select
+                        id="programa"
+                        label="Programa"
+                        name="programa"
+                        defaultValue={data.programa || 'verde'} 
+                        disabled 
+                      >
+                        <MenuItem value={1}>Verde</MenuItem>
+                        <MenuItem value={2}>Amarelo</MenuItem>
+                        <MenuItem value={3}>Vermelho</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </Grid>
+                <Grid component="div" size={6}>
+                  <Box sx={{ minWidth: 120}}>
+                    <FormControl fullWidth>
+                      <InputLabel id='orgaoresponsavel'>Órgão responsável</InputLabel>
+                      <Select
+                        id="orgaoresponsavel"
+                        label="Órgão responsável"
+                        name="orgao"
+                        defaultValue={data.orgao || 'verde'}  
+                        disabled
+                        >
+                        <MenuItem value={1}>Verde</MenuItem>
+                        <MenuItem value={2}>Amarelo</MenuItem>
+                        <MenuItem value={3}>Vermelho</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </Grid>
+                <Grid component="div" size={12}>
+                  <TextField 
+                    fullWidth
+                    multiline
+                    rows={4}
+                    label='Descrição' 
+                    name="descricao"
+                    defaultValue={data.descricao}  
+                    disabled
+                    />
+                </Grid>
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
         </Grid>
         <Grid component="div" size={6}>
           <TextField 
             fullWidth 
-            label='Data de registro' 
-            name="data_registro" 
-            defaultValue={data.data_registro}
             disabled={isEdit}
-            />
-        </Grid>
-        <Grid size={12} component="div">
-          <TextField 
-            fullWidth 
-            name="titulo" 
-            label='Título' 
-            defaultValue={data.titulo}
-            disabled={isEdit}
-            />
-        </Grid>
-        <Grid component="div" size={4}>
-          <Box sx={{ minWidth: 120}} >
-            <FormControl fullWidth>
-              <InputLabel id='fornecedor'>Fornecedor</InputLabel>
-              <Select
-                id="fornecedor"
-                label="Fornecedor"
-                name="fornecedor"
-                defaultValue={data.fornecedor || '1'}
-                disabled={isEdit}
-              >
-                <MenuItem value={1}>Verde</MenuItem>
-                <MenuItem value={2}>Amarelo</MenuItem>
-                <MenuItem value={3}>Vermelho</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Grid>
-        <Grid component="div" size={4}>
-            <TextField 
-              fullWidth 
-              label='Data' 
-              name="data_data" 
-              defaultValue={data.data_data}
-              disabled={isEdit}
-              />
-        </Grid>
-        <Grid component="div" size={4}>
-          <Box sx={{ minWidth: 120}}>
-            <FormControl fullWidth>
-              <InputLabel id='prioridade'>Prioridade</InputLabel>
-              <Select
-                id="prioridade"
-                label="Prioridade"
-                name="prioridade"
-                defaultValue={data.prioridade || 'verde'}
-                disabled={isEdit}
-                >
-                <MenuItem value={'verde'}>Verde</MenuItem>
-                <MenuItem value={'amarelo'}>Amarelo</MenuItem>
-                <MenuItem value={'vermelho'}>Vermelho</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Grid>
-        <Grid component="div" size={6}>
-          <TextField 
-            fullWidth 
-            label='Distrito'
-            name="distrito"
-            defaultValue={data.distrito}
-            disabled={isEdit}
-            />
-        </Grid>
-        <Grid component="div" size={6}>
-          <TextField 
-            fullWidth 
-            label='Bairro'
-            name="bairro"
-            defaultValue={data.bairro}  
-            disabled={isEdit}
-            />
-        </Grid>
-        <Grid component="div" size={12}>
-          <TextField 
-            fullWidth 
-            label='Logradouro' 
-            name="logradouro"
-            defaultValue={data.logradouro}  
-            disabled={isEdit}
-            />
-        </Grid>
-        <Grid component="div" size={6}>
-          <Box sx={{ minWidth: 120}}>
-            <FormControl fullWidth>
-              <InputLabel id='programa'>Programa</InputLabel>
-              <Select
-                id="programa"
-                label="Programa"
-                name="programa"
-                defaultValue={data.programa || 'verde'} 
-                disabled={isEdit} 
-              >
-                <MenuItem value={1}>Verde</MenuItem>
-                <MenuItem value={2}>Amarelo</MenuItem>
-                <MenuItem value={3}>Vermelho</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Grid>
-        <Grid component="div" size={6}>
-          <Box sx={{ minWidth: 120}}>
-            <FormControl fullWidth>
-              <InputLabel id='orgaoresponsavel'>Órgão responsável</InputLabel>
-              <Select
-                id="orgaoresponsavel"
-                label="Órgão responsável"
-                name="orgao"
-                defaultValue={data.orgao || 'verde'}  
-                disabled={isEdit}
-                >
-                <MenuItem value={1}>Verde</MenuItem>
-                <MenuItem value={2}>Amarelo</MenuItem>
-                <MenuItem value={3}>Vermelho</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Grid>
-        <Grid component="div" size={12}>
-          <TextField 
-            fullWidth
-            multiline
-            rows={4}
-            label='Descrição' 
-            name="descricao"
-            defaultValue={data.descricao}  
-            disabled={isEdit}
-            />
-        </Grid>
-        <Grid component="div" size={6}>
-          <TextField 
-            fullWidth 
-            disabled={isEdit} 
             name="orcamentoprevisto"
             label='Orçamento previsto'
             defaultValue={data.orcamentoprevisto}  
